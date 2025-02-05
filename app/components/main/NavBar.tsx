e1958e9ac43e2fd125ee5274801368a7dccf34f4
Mehmet YILDIZ <memostar91@gmail.com>
2025-02-05 07:35:01 +0300

    Preparing for v1.0.0 release Both mobile and desktop versions are now complete for 1.0.0 release

--- app/components/main/NavBar.tsx
+++ app/components/main/NavBar.tsx
@@ -6,6 +6,14 @@ import Image from "next/image";
 import { Menu } from "lucide-react";
 import { ThemeToggle } from "./ThemeToggle";
 import { useTheme } from "next-themes";
+import {
+  FaLinkedinIn,
+  FaTwitter,
+  FaFacebookSquare,
+  FaTelegram,
+  FaEnvelope,
+  FaWindowClose,
+} from "react-icons/fa";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,12 +31,34 @@ export default function Navbar() {
     };
     window.addEventListener("scroll", handleShadow);
   });
+  const socialLinks = [
+    {
+      href: "https://www.linkedin.com/in/yildizmehmet/",
+      icon: <FaLinkedinIn className="text-gold text-lg" />,
+    },
+    {
+      href: "https://twitter.com/albursavi",
+      icon: <FaTwitter className="text-gold text-lg" />,
+    },
+    {
+      href: "https://www.facebook.com/mehmetydev/",
+      icon: <FaFacebookSquare className="text-gold text-lg" />,
+    },
+    {
+      href: "https://t.me/memostar91",
+      icon: <FaTelegram className="text-gold text-lg" />,
+    },
+    {
+      href: "mailto:contact@mehmetyildiz.dev",
+      icon: <FaEnvelope className="text-gold text-lg" />,
+    },
+  ];
 
   return (
     <div
       className={
         shadow
-          ? "fixed w-full h-16 shadow-[0_15px_10px_-15px_rgba(220,177,24,0.4)] z-100 bg-diamond"
+          ? "fixed w-full h-16 shadow-[0_15px_10px_-15px_rgba(220,177,24,0.7)] z-100 bg-diamond"
           : "fixed w-full h-16 z-100"
       }
     >
@@ -54,18 +84,30 @@ export default function Navbar() {
             </Link>
             <ThemeToggle />
           </div>
-          <div className="flex items-center">
-            <button onClick={() => setIsOpen(true)} className="md:hidden p-2">
-              <Menu className="h-5 w-5" />
+          <div className="md:hidden mr-4 flex items-center">
+            <ThemeToggle />
+            <button onClick={() => setIsOpen(true)} className="p-2">
+              <Menu className="ml-2 h-5 w-5" />
             </button>
           </div>
         </div>
         {isOpen && (
-          <div className="fixed inset-0 z-50 flex">
-            <div className="bg-white dark:bg-gray-800 p-4 w-64 h-full shadow-lg transform transition-transform duration-300 ease-in-out translate-x-0">
-              <button className="absolute top-4 right-4" onClick={onClose}>
-                Close
-              </button>
+          <div className="fixed inset-0 z-999 flex">
+            <div className="bg-pearl px-4 py-2 w-4/5 h-full shadow-lg transform transition-transform duration-1500 ease-in-out translate-x-0">
+              <div className="h-14 pb-2 border-gold border-b-2">
+                <Link href="/">
+                  <Image
+                    className="pr-8"
+                    src={theme === "dark" ? "/logo_l.svg" : "/logo_d.svg"}
+                    alt="Logo"
+                    width={160}
+                    height={48}
+                  />
+                </Link>
+                <button className="absolute top-4 right-4" onClick={onClose}>
+                  <FaWindowClose className="text-3xl text-gold" />
+                </button>
+              </div>
               <div className="flex flex-col items-start mt-8">
                 <Link href="/" className="mb-4" onClick={onClose}>
                   Home
@@ -74,8 +116,26 @@ export default function Navbar() {
                   About
                 </Link>
               </div>
+              <div className="pt-64 text-gold">
+                <p>Let&apos;s Connect!</p>
+              </div>
+              <div className="flex items-center justify-between py-6">
+                {socialLinks.map((link, index) => (
+                  <a
+                    key={index}
+                    href={link.href}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    title={link.href.replace(/^https?:\/\//, "").split("/")[0]} // Extracts domain name
+                  >
+                    <div className="rounded-full shadow-lg shadow-gold cursor-pointer p-3">
+                      {link.icon}
+                    </div>
+                  </a>
+                ))}
+              </div>
             </div>
-            <div className="grow bg-black bg-opacity-50" onClick={onClose} />
+            <div className="grow bg-sapphire/90" onClick={onClose} />
           </div>
         )}
       </div>
