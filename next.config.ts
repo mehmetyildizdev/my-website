fa51913fc447b1acd8feff80237730f6d467c046
Mehmet YILDIZ <memostar91@gmail.com>
2026-04-26 19:01:28 +0300

    Package updates and config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
   /* config options here */
 };
