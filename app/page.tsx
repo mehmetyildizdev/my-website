9c180ba8ab3de4b660fb57ef54075a7af47d1414
Mehmet YILDIZ <memostar91@gmail.com>
2026-05-16 23:17:52 +0300

    refactor(home): improve the styling and remove about me section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Opening from "@/components/homepage/Opening";
-import Summary from "@/components/homepage/Summary";
+import Opening from "@/components/Opening";
 
 export default function Home() {
   return (
-    <section className="bg-diamond pt-16">
+    <main className="h-dvh max-h-dvh overflow-hidden bg-diamond">
       <Opening id="hero" />
-      <Summary id="aboutme" />
-    </section>
+    </main>
   );
 }
